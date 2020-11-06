025f272f1be55fd403c31458e75c07e22410901a
Andrey Pereslavtsev <pereslavtsev@yandex.ru>
2020-11-06 04:38:29 +0300

    ISAF XRR Schema and Layout training and drafting

--- Match Race Officer/ViewController.swift
+++ Match Race Officer/ViewController.swift
@@ -7,11 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = indexPath.row.isMultiple(of: 2) ? .green : .cyan
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell
+    }
+    
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var splash: UIImageView!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = "Функция: \(#function)"
+        label.textColor = .blue
+        
+        splash.image = UIImage(named: "splash")
+        
+        if let text = textView.text {
+            textView.text = String(text.reversed())
+        }
+        
+        // UIScrollView
+        let imageView = UIImageView(image: UIImage(named: "splash"))
+        let scrollView = UIScrollView(frame: CGRect(x: view.bounds.midX - 200, y: 100, width: 400, height: 200) )
+        scrollView.backgroundColor = .blue
+        scrollView.contentSize = imageView.bounds.size
+        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        scrollView.addSubview(imageView)
+        view.addSubview(scrollView)
+        view.sendSubviewToBack(scrollView)
+        
+        tableView.backgroundColor = .cyan
+        tableView.dataSource = self
+        
     }
 
 
